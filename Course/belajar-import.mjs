26bee96278b0000205c61066131098852078bc7b
nadiarizkyhairunnisa <09021281823039@student.unsri.ac.id>
2025-07-02 07:10:39 +0700

    add course material

---
+++ Course/belajar-import.mjs
@@ -0,0 +1,5 @@
+import { name, favoriteFood as food, sayHi } from "./module.mjs";
+
+console.log(name);
+console.log(food);
+sayHi(name);
---
+++ Course/module.mjs
@@ -0,0 +1,6 @@
+export const name = "John";
+export const favoriteFood = ["pizza", "pasta", "sushi"];
+
+export function sayHi(name) {
+  console.log(`Hi, ${name}!`);
+}
