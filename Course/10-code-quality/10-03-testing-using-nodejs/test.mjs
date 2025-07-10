a04f69b761356ae44235083d5c33a647b707415a
nadiarizkyhairunnisa <09021281823039@student.unsri.ac.id>
2025-07-10 14:42:49 +0700

    docs(test): add comment for expected output

--- Course/10-code-quality/10-03-testing-using-nodejs/test.mjs
+++ Course/10-code-quality/10-03-testing-using-nodejs/test.mjs
@@ -49,4 +49,19 @@ describe("Calculator", () => {
     // To run this test, use the command: node --test test.mjs
     // Karena kalo langsung run dari IDE, 
     // it just executed your code like a regular script without invoking the test runner output mode.
+
+    //Expected output:
+    ▶ Calculator
+        ✔ should add correctly (1.5239ms)
+        ✔ should throw an error if string passed on numA parameter (1.1005ms)
+        ✔ should throw an error if string passed on numB parameter (0.3891ms)
+    ✔ Calculator (5.5367ms)
+    ℹ tests 3
+    ℹ suites 1 //one group of tests (using describe)
+    ℹ pass 3
+    ℹ fail 0
+    ℹ cancelled 0
+    ℹ skipped 0
+    ℹ todo 0
+    ℹ duration_ms 203.2876
 */
