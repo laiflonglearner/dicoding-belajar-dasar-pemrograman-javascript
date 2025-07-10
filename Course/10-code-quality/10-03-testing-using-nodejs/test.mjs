62033dc2f9996efe5abad8501f7204bc4eac7038
nadiarizkyhairunnisa <09021281823039@student.unsri.ac.id>
2025-07-10 14:20:28 +0700

    chore: add input validation and error test for add function

--- Course/10-code-quality/10-03-testing-using-nodejs/test.mjs
+++ Course/10-code-quality/10-03-testing-using-nodejs/test.mjs
@@ -15,8 +15,36 @@ test("should add correctly", () => {
   assert.equal(actualValue, expectedValue);
 });
 
+test("should throw an error if string passed on numA parameter", () => {
+  const potentialErrorToBeThrew = () => {
+    // Arrange
+    const operandA = "5";
+    const operandB = 4;
+
+    // Action
+    add(operandA, operandB);
+  };
+
+  // Assert
+  assert.throws(potentialErrorToBeThrew, Error);
+});
+
+test("should throw an error if string passed on numB parameter", () => {
+  const potentialErrorToBeThrew = () => {
+    // Arrange
+    const operandA = 10;
+    const operandB = "8";
+
+    // Action
+    add(operandA, operandB);
+  };
+
+  // Assert
+  assert.throws(potentialErrorToBeThrew, Error);
+});
+
 /*
-// To run this test, use the command:
-// node --test test.mjs
-// Karena kalo langsung run dari IDE, it just executed your code like a regular script without invoking the test runner output mode.
+    // To run this test, use the command: node --test test.mjs
+    // Karena kalo langsung run dari IDE, 
+    // it just executed your code like a regular script without invoking the test runner output mode.
 */
