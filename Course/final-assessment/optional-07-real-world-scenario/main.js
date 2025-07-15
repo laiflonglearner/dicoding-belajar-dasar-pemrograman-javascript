9ae96b90b8e303dbfbc03793dc01c5548d33cb8e
nadiarizkyhairunnisa <09021281823039@student.unsri.ac.id>
2025-07-15 15:03:36 +0700

    docs(feat): add 07-real-world-scenario solution

--- Course/final-assessment/optional-07-real-world-scenario/main.js
+++ Course/final-assessment/optional-07-real-world-scenario/main.js
@@ -1,17 +1,24 @@
-import { orders, addOrder, updateOrderStatus, calculateTotalRevenue, deleteOrder } from './orders.js';
+import {
+  orders,
+  addOrder,
+  displayOrder,
+  updateOrderStatus,
+  calculateTotalRevenue,
+  deleteOrder,
+} from "./orders.js";
 
-addOrder('Alice', [
-  { name: 'Nasi Goreng', price: 20000 },
-  { name: 'Teh Manis', price: 5000 }
+addOrder("Alice", [
+  { name: "Nasi Goreng", price: 20000 },
+  { name: "Teh Manis", price: 5000 },
 ]);
 
-addOrder('Bob', [
-  { name: 'Mie Goreng', price: 15000 },
-  { name: 'Kopi', price: 10000 }
+addOrder("Bob", [
+  { name: "Mie Goreng", price: 15000 },
+  { name: "Kopi", price: 10000 },
 ]);
 
-console.log(orders);
-
+console.log("List Pesanan:");
+console.log(displayOrder());
 /**
  * Output yang diharapkan:
  * [
@@ -38,10 +45,11 @@ console.log(orders);
  * ]
  */
 
-updateOrderStatus(orders[0].id, 'Diproses');
-updateOrderStatus(orders[1].id, 'Selesai');
+updateOrderStatus(orders[0].id, "Diproses");
+updateOrderStatus(orders[1].id, "Selesai");
 
-console.log(orders);
+console.log("List Pesanan:");
+console.log(displayOrder());
 
 /**
  * Output yang diharapkan:
@@ -69,16 +77,17 @@ console.log(orders);
  * ]
  */
 
+console.log("Total Revenue:");
 console.log(calculateTotalRevenue());
 /**
  * Output yang diharapkan:
  * 25000
  */
 
-
 deleteOrder(orders[0].id);
 
-console.log(orders);
+console.log("List Pesanan:");
+console.log(displayOrder());
 
 /**
  * Output yang diharapkan:
--- Course/final-assessment/optional-07-real-world-scenario/orders.js
+++ Course/final-assessment/optional-07-real-world-scenario/orders.js
@@ -3,20 +3,63 @@ function generateUniqueId() {
   return `_${Math.random().toString(36).slice(2, 9)}`;
 }
 
-
 // TODO: buatlah variabel yang menampung data orders
-let orders;
+let orders = [];
 
 // TODO: selesaikan fungsi addOrder
-function addOrder(customerName, items) {}
+function addOrder(customerName, items) {
+  let order = {
+    id: generateUniqueId(),
+    customerName,
+    items: items,
+    totalPrice: items.reduce(
+      (accumulator, item) => accumulator + item.price,
+      0
+    ),
+    status: "Menunggu",
+  };
+
+  orders.push(order);
+}
 
 // TODO: selesaikan fungsi updateOrderStatus
-function updateOrderStatus(orderId, status) {}
+function updateOrderStatus(orderId, status) {
+  orders = orders.map((order) => {
+    if (order.id === orderId) {
+      return { ...order, status };
+    }
+    return order;
+  });
+  console.log(
+    `Status order dengan ID ${orderId} telah diperbarui menjadi ${status}.`
+  );
+}
 
 // TODO: selesaikan fungsi calculateTotalRevenue dari order yang berstatus Selesai
-function calculateTotalRevenue() {}
+function calculateTotalRevenue() {
+  return orders.reduce((accumulator, order) => {
+    if (order.status === "Selesai") {
+      return accumulator + order.totalPrice;
+    }
+    return accumulator;
+  }, 0);
+}
 
 // TODO: selesaikan fungsi deleteOrder
-function deleteOrder(id) {}
+function deleteOrder(id) {
+  orders = orders.filter((order) => order.id !== id);
+  console.log(`Order dengan ID ${id} telah dihapus.`);
+}
 
-export { orders, addOrder, updateOrderStatus, calculateTotalRevenue, deleteOrder };
+function displayOrder() {
+  return JSON.stringify(orders, null, 2);
+}
+
+export {
+  orders,
+  addOrder,
+  updateOrderStatus,
+  calculateTotalRevenue,
+  deleteOrder,
+  displayOrder,
+};
